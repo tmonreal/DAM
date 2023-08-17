49ce162f95fba047cd4905b71e0ec5dcdbceac8b
tmonreal <trinidadmonreal@gmail.com>
2023-08-17 00:38:41 -0300

    sensor page updated

--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/dispositivos/dispositivos.page.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/dispositivos/dispositivos.page.ts
@@ -12,21 +12,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class DispositivosPage implements OnInit, OnDestroy {
-  public dispositivo = new Dispositivo(0, " ", " ", 0);
-  public medicion = new Medicion(0, ' ', 0, 0);
+  public dispositivo = new Dispositivo(0, "", "", 0);
+  public medicion = new Medicion(0, "", 0, 0);
   public dispositivoId: number;
 
-  observable$: Observable<any>
-  subscription: Subscription
+  //observable$: Observable<any>
+  //subscription: Subscription
 
   constructor(private router: ActivatedRoute, private _dispositivoService: DispositivoService) {
-    this.observable$ = interval(1000)
+    //this.observable$ = interval(1000)
 
-    const move_values = this.observable$.pipe(map(val => val+10))
+    //const move_values = this.observable$.pipe(map(val => val+10))
 
-    this.subscription = move_values.subscribe((value) => {
-      console.log(value)
-    })
+    //this.subscription = move_values.subscribe((value) => {
+    //  console.log(value)
+    //})
   }
 
   //mouseMove = fromEvent(document, 'mousemove')
@@ -43,35 +43,29 @@ export class DispositivosPage implements OnInit, OnDestroy {
       console.log(this.dispositivoId);
     });
 
-    this.subscription.unsubscribe()
+    await this._dispositivoService.getDispositivosById(this.dispositivoId)
+    .then(dispo => {this.dispositivo = dispo} )
+    .catch((error) => {
+      console.log('Error: ', error)
+    })
 
-    await this._dispositivoService.getListadoDispositivos()
-      .then((dispositivos) => {
-        for (let dispositivo of dispositivos){
-          console.log(dispositivo.nombre)
-        }
-        console.log(dispositivos)
-      })
-      .catch((error) => {
-        console.log('Error:', error)
-      })
-    // Tarda mucho mas en resolverse la consulta a la API que hacer un console log
-    // Si uso async - await esperamos a que termine de ejecutar la promesa
-    console.log('Me ejecuto primero')
+    // esto me devuelve undefined
+    console.log(this.dispositivo.ubicacion)
+    console.log("Type of this.dispositivo:", typeof this.dispositivo);
   }
 
   subscribe () {
-    this.subscription = this.observable$.subscribe((integer) => {
-      console.log(integer)
-    })
+    //this.subscription = this.observable$.subscribe((integer) => {
+    //  console.log(integer)
+  //  })
   }
 
   unsubscribe () {
-    this.subscription.unsubscribe()
+    //this.subscription.unsubscribe()
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe()
+    //this.subscription.unsubscribe()
     //this.subscription_mouse.unsubscribe()
   }
 }
