c7b9424ecbea1ba39527fb6e21367b2eb21a4466
tmonreal <trinidadmonreal@gmail.com>
2023-08-28 13:50:09 -0300

    fixed fecha format like db

--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/pipes/fecha-format.pipe.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/pipes/fecha-format.pipe.ts
@@ -8,7 +8,7 @@ export class FechaFormatPipe implements PipeTransform {
 
   transform(value: any): any {
     const new_fecha = new DatePipe('en-US');
-    return new_fecha.transform(value, "dd-MM-yyyy HH:mm:ss", "UTC");
+    return new_fecha.transform(value, "yyyy-MM-dd HH:mm:ss", "UTC");
   }
 
 
